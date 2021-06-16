2f2eb3179a23f6b0b7aaca33faee885274a41955
Wyatt Kirby <wyatt@apsis.io>
2021-06-16 14:40:12 -0400

    Add nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,13 @@
+import Router from "next/router";
+import NProgress from "nprogress";
+
+import "nprogress/nprogress.css";
 import "styles/main.scss";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }

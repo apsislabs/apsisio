b71555ddb8cc7e359da20c430393bcda431072e8
Wyatt Kirby <wyatt@apsis.io>
2024-09-22 20:39:28 -0400

    Add favicon and sitemap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ export const mono = IBM_Plex_Mono({
 
 import "nprogress/nprogress.css";
 import "styles/main.scss";
+import Head from "next/head";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
@@ -28,6 +29,27 @@ export default function MyApp({ Component, pageProps }) {
           --font-mono: ${mono.style.fontFamily};
         }
       `}</style>
+
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
 
       <Script
         defer

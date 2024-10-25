810c1a83c31ae41c7997395eaf3fb401a8f3a161
Wyatt Kirby <wyatt@apsis.io>
2024-10-25 10:14:52 -0400

    Fix social image

--- src/components/PageMeta.tsx
+++ src/components/PageMeta.tsx
@@ -38,12 +38,12 @@ export const PageMeta: React.FC<{ title?: string; description?: string }> = ({
     <meta property="og:type" content="website" />
     <meta property="og:title" content={title ?? siteConf.meta.title} />
     <meta property="og:description" content={description ?? siteConf.meta.description} />
-    <meta property="og:image" content="https://apsis.io/social.jpg" />
+    <meta property="og:image" content="https://apsis.io/social.png" />
     <meta name="twitter:card" content="summary_large_image" />
     <meta property="twitter:domain" content="apsis.io" />
     <meta property="twitter:url" content="https://apsis.io" />
     <meta name="twitter:title" content={title ?? siteConf.meta.title} />
     <meta name="twitter:description" content={description ?? siteConf.meta.description} />
-    <meta name="twitter:image" content="https://apsis.io/social.jpg" />
+    <meta name="twitter:image" content="https://apsis.io/social.png" />
   </>
 );

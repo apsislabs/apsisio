d30552a063d5dc79cb654c42a20c37952af5301e
wkirby <wkirby@users.noreply.github.com>
2024-12-06 21:34:39 +0000

    Deploying to gh-pages from @ apsislabs/apsisio@4d5f5c68d2fd8fc329fb75d190c861a7bb07d67f 🚀

---
+++ _next/static/ArBnZ9B7fMn2louPQ4Dqp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fblog","\u002Fblog\u002F[year]\u002F[month]\u002F[day]\u002F[slug]","\u002Fblog\u002Fpeople\u002F[person]","\u002Fconduct","\u002Fmdbn","\u002Fprivacy","\u002Fteam"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()

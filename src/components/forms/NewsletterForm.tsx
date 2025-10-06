59910b3a9dc5b0d6c8c81d7afd02900797d742bb
Wyatt Kirby <wyatt@apsis.io>
2025-10-06 15:46:27 -0400

    Fix newsletter form ID

--- src/components/forms/NewsletterForm.tsx
+++ src/components/forms/NewsletterForm.tsx
@@ -27,7 +27,7 @@ export const NewsletterForm = () => {
         id="00924"
         type="hidden"
         name="l"
-        value="00924ffd-3d44-4d55-8487-36beb0786966"
+        value="f2138cbb-fad3-499e-b5d5-b43b1c8accfb"
       />
 
       <TextField
